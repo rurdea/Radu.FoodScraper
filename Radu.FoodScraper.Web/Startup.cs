0469ee005fc0ef7cd0038839da3adffbcfb36aab
radu <radu.urdea@teamery.com>
2020-09-26 00:12:24 +0700

    added swagger ui;

--- Radu.FoodScraper.Web/Startup.cs
+++ Radu.FoodScraper.Web/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Radu.FoodScraper.Data.Sql;
 using Radu.FoodScraper.Web.Services;
+using System.Linq;
 
 namespace Radu.PureScraper.Web
 {
@@ -29,6 +30,18 @@ namespace Radu.PureScraper.Web
             var connectionString = Configuration["Data:ConnectionString"];
 
             services.AddTransient(d => new DataService(connectionString, d.GetService<ILogger<DataService>>()));
+
+            services.AddSwaggerGen(c =>
+            {
+                var assembly = this.GetType().Assembly;
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = assembly.GetName().Name,
+                    Description = assembly.FullName
+                }); ;
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +61,17 @@ namespace Radu.PureScraper.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger(c =>
+            {
+                c.RouteTemplate = "help/{documentName}/swagger.json";
+            });
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "help";
+                var endpoint = "/help/v1/swagger.json";
+                c.SwaggerEndpoint(endpoint, "Radu.FoodScraper.Web");
             });
         }
     }
